9bb0828ae34585a616809a9c7cb4ed1a27b0701a
benjamin-nartey <nartbillionz@gmail.com>
2023-08-06 16:48:25 +0000

    complete About section and add to page

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,13 @@
-'use client';
+"use client";
+import { useState } from "react";
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+import { motion } from "framer-motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+
+const Explore = () => <section>Explore section</section>;
 
 export default Explore;
